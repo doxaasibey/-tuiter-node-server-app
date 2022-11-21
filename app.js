931db75d0b70703b4e4453796034bc992cb9c9cb
asibeyd <asibey.d@northeastern.edu>
2022-11-20 19:31:44 -0500

    package commit fix

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ import cors from 'cors';
 
 import UserController from "./controllers/users/users-controller.js";
 import HelloController from "./controllers/hello-controller.js";
-import TuitsController
-    from "./controllers/tuits/tuits-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 TuitsController(app);
