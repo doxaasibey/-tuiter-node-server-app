053f281681554e314095fd329074cdc3338668c7
asibeyd <asibey.d@northeastern.edu>
2022-12-03 01:11:59 -0500

    "a9 deploy"

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const schema = mongoose.Schema({
+        tuit: String,
+        username: String,
+        image: String,
+        time: String,
+        likes: Number,
+        liked: Boolean,
+        dislikes: Number,
+        disliked: Boolean
+},
+    {collection: 'tuits'});
+
+export default schema;
